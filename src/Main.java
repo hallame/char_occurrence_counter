841a382822a5fc0f7cbd7fe12ef6d3e8c29e7133
hallame <hormiseallame5@gmail.com>
2023-09-24 10:26:36 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+//import java.lang.*;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int nb = 0;
+//        String str;
+//        char ch2;
+//        char maj = (char) 83;
+//        char mij = (char) 115;
+//        System.out.println("Enter un word:");
+//        str = in.nextLine();
+//        String str2 = "";
+//
+//            for(int i=0; i<str.length(); i++) {
+//                char ch1 = str.charAt(i);
+//                if(ch1 == maj) {
+//                    ch2 = mij;
+//                    nb += 1;
+//                }else{
+//                    ch2 = ch1;
+//
+//                }
+//                str2 += ch2;
+//            }
+//        System.out.println("Answer Task3:" + str2)
+import java.lang.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int it = 0, nb = 0;
+        String str, ch0, str2 = "", vide = "";
+        System.out.println("Enter the letter of interest:");
+        ch0 = in.nextLine();
+        String ch4 = ch0.toUpperCase();
+        String ch5 = ch0.toLowerCase();
+        char ch2 = ch4.charAt(0);
+        char ch3 = ch5.charAt(0);
+        System.out.println("Enter un word:");
+        str = in.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch1 = str.charAt(i);
+            if (ch1 == ch2 || ch1 == ch3) {
+                nb += 1;
+                it += 1;
+                if (nb == 1) {
+                    if (i == str.length() - 1) { // if the character is the last one in the string, no need compare to next one, so we stop
+                        str2 += ch1;
+                        str2 += "{" + nb + "}";
+                    } else {
+                        if (ch1 == str.charAt(i + 1)) {
+                            str2 += ch1;
+                        } else {
+                            str2 += ch1;
+                            str2 += "{" + nb + "}";
+                            nb = 0;
+                        }
+                    }d
+                } else if (nb >= 2) {
+                    if (i == str.length()) {
+                        str2 += "{" + nb + "}";
+                    } else {
+                        if (ch1 == str.charAt(i + 1)) {
+                            str2 += vide;
+                        } else {
+                            str2 += "{" + nb + "}";
+                            nb = 0;
+                        }
+                    }
+                }
+            }else{
+                    str2 += ch1;
+                }
+        }
+            if (it == 0) {
+                System.out.println("There isn't " + ch2 + " in " + str);
+            } else {
+                System.out.println("Occurence of " + ch2 + "/" + ch3+":");
+                System.out.println(str2);
+                //System.out.println(it+" "+ch2);
+            }
+        }
+    }
---
+++ src/toUpperKase.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class toUpperKase {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            String str1, str2 = "";
+            String str0;
+            System.out.println("The letter you want to put to UpperCase: ");
+            str0 = in.nextLine();
+            char ch0 = str0.charAt(0);
+            String ch1 = str0.toUpperCase();
+            String ch2 = str0.toLowerCase();
+            char ch3 = ch1.charAt(0);
+            char ch4 = ch2.charAt(0);
+            System.out.println("Enter a word: ");
+            str1 = in.nextLine();
+
+            for(int i=0; i<str1.length(); i++){
+                char ch5 = str1.charAt(i);
+                if(ch5 == ch3 || ch5 == ch4){
+                    str2 += ch3;
+
+                }else{
+                    str2 += ch5;
+
+                }
+            }
+            System.out.println(str1+ " with '" + ch0 + "' toUpperCase:");
+            System.out.println(str2);
+        }
+    }
+
